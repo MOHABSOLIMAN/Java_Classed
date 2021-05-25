d829feef5f18c3e18f8741a0dee5ada019adb509
Mohab Soliman <mohabsouliman3@gmail.com>
2021-05-25 05:43:44 +0200

    first commit

---
+++ Thread/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ShutDown s1=new ShutDown();
+        ShutDown s2=new ShutDown();
+
+        s1.start();
+        s2.start();
+    }
+}
