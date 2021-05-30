e6970fa11175b32bed91255d0325da2bc857b1ba
Mohab Soliman <mohabsouliman3@gmail.com>
2021-05-30 21:47:58 +0200

    First Commit

---
+++ StreamLab3_3/src/countriesPopAvgClass.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class countriesPopAvgClass {
+	public Double countriesPopAvg(List<Country> countries){
+        return countries.stream().map(Country::getPopulation).mapToInt((x)->x).average().getAsDouble();
+    }
+	public int countriesPopMax(List<Country> countries){
+        return countries.stream().map(Country::getPopulation).mapToInt((x)->x).max().getAsInt();   
+    }
+}
