2754b1212dbc9d4d64d8d9c0ee08b51afbd761ef
Mohab Soliman <mohabsouliman3@gmail.com>
2021-06-11 18:34:00 +0200

    first commit

---
+++ charts/src/charts/MAin.java
@@ -0,0 +1,34 @@
+package charts;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import joinery.DataFrame;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.io.csv.CsvReadOptions;
+import tech.tablesaw.io.csv.CsvReader;
+
+public class MAin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String filename = "src/main/resources/titanic-passengers.csv";
+    // make some statistic using joinery
+        TittianicDOA.readcsvujoinery(filename);
+        // make some statistic using tablesaw
+        TittianicDOA.readcsvuTableSaw(filename);
+        
+//		 testing reading json file using jackson and ploting using xchart 
+
+		TittianicDOA tittianicDOA = new TittianicDOA();
+        List<TitanicPassenger> passengers =tittianicDOA.getPassengersFromJsonFile();
+        tittianicDOA.graphPassengerAgesDraw(passengers);
+        tittianicDOA.graphPassengerClass(passengers);
+        tittianicDOA.graphPassengerSurvived(passengers);
+        tittianicDOA.graphPassengerSurvivedGender(passengers);
+        
+        System.out.println("test"+passengers.get(0).getName());
+		
+	}
+
+}
